0a2c0328f5f0907135c4d92417a639bf75040a59
robemike <robemike1135@gmail.com>
2024-05-30 06:51:20 +0300

    makes rectification.

--- lib/student.py
+++ lib/student.py
@@ -11,12 +11,11 @@ class ChattyStudent(Student):
         super().hello()
         print("How are you doing today? I'm okay, but I'm kind of tired. Did you watch The Walking Dead last night? You didn't?! Oh man, it was so crazy! What, you don't want any spoilers? Okay well let me just tell you who died...")
 
-    def raise_hand(self):
+    def raise_hand(self):  
         for i in range(10):
-            print("Pick me!")
+            super().raise_hand()
         pass
     pass
 
 chatty_student = ChattyStudent()
-print(chatty_student)
 chatty_student.raise_hand()
